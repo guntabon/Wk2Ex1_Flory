fecfe79e69474ede90e68a697ed42e763161fb15
gunne <gunne@GUNNY>
2025-01-26 22:00:35 -0600

    Add project files.

---
+++ Wk2Ex1_Flory/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wk2Ex1_Flory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
